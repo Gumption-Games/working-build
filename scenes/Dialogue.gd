bfbf2d3ee1839b16a78856c692b15c5f778be94b
hughbagan <hbagan@ualberta.ca>
2020-02-27 10:42:41 -0700

    Hotstuff for the Demo

--- scenes/Dialogue.gd
+++ scenes/Dialogue.gd
@@ -2,4 +2,4 @@ extends Label
 
 
 func _ready():
-	set_text('Place the ingredients in the cauldron.\nThen click the cauldron.')
+	set_text("Place the ingredients in the cauldron.\nThen click the cauldron.\nClick and drag in circles to stir the pot.\nDon't let it burn!")
