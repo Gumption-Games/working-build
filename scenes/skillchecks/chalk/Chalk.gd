ba37bb38604a4343d09660413e5d2285ccdbf6f2
aridgway120 <aridgway@ualberta.ca>
2020-03-03 18:27:12 -0700

    Basic chalk drawing

---
+++ scenes/skillchecks/chalk/Chalk.gd
@@ -0,0 +1,28 @@
+class_name Chalk extends Line2D
+
+
+var dragging = false
+var starting_point = Vector2(40, 100)
+
+
+func _ready():
+	$ChalkIcon.position = starting_point
+
+
+func _input(event):
+#	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+#		if event.pressed:
+#			print("dragging")
+#			# Chalk is picked up
+#			dragging = true
+#		else:
+#			# Chalk is dropped
+#			dragging = false
+	if $ChalkIcon.dragging and event is InputEventMouseMotion:
+		add_point(event.position)
+
+
+func reset():
+	OS.delay_msec(400)
+	clear_points()
+	$ChalkIcon.position = starting_point
---
+++ scenes/skillchecks/chalk/ChalkIcon.gd
@@ -0,0 +1,18 @@
+extends "res://scenes/ingredients/Ingredient.gd"
+
+var chalk
+
+
+func _ready():
+	chalk = get_parent()
+
+
+func _on_ChalkIcon_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if event.pressed:
+			# Object is picked up
+			dragging = true
+		else:
+			# Held object is being dropped
+			dragging = false
+			chalk.reset()
