503fdab193d4a66a060df9574803aa22bd9b5a3c
hughbagan <hbagan@ualberta.ca>
2020-03-11 14:41:39 -0600

    Added tool tips to Ingredients - Mostly implemented in Ingredient class, and method called in subclass ingredients - Modifications to the Shelf and Workbench to accomodate

--- scenes/ingredients/SnakeOil.gd
+++ scenes/ingredients/SnakeOil.gd
@@ -3,3 +3,6 @@ class_name SnakeOil extends Ingredient
 func _init():
 	#IMG_PATH = ".import/blue_square.png-771ba9c0f571bad09ff2a9ed396e4ad9.stex"
 	type = "SnakeOil"
+
+func _ready():
+	_set_label("Snake Oil")
