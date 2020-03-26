fe85cdb5369e0654854fb0944c7b65f930e0684b
Jacob <jblazusi@ualberta.ca>
2020-03-25 19:32:11 -0600

    Updated arm movement

--- scenes/combiners/Knife.gd
+++ scenes/combiners/Knife.gd
@@ -3,82 +3,60 @@ class_name Knife extends Combiner
 var CutPointScene := preload("res://scenes/combiners/CutPoint.tscn")
 
 # Play with these values to change the game
-export var hit_area_width := 10
-export var hit_area_speed : float = 1.0
-export var amplitude_factor : float = 1.5
-export var num_cuts : int = 3
-export var cutpoint_width : float = 4.0
+export var speed : int = 100
+export var direction : int = 1
 
-var sine_x : float = -PI/2.0 # see hitarea.pos formula in _process()
-const SPEED : int = 30
-
+# Accessing nodes in scene
 onready var label := $Label
+onready var wheel := $CelestialWheel
 onready var arm := $CelestialWheel/Arm
 onready var hitarea := $CelestialWheel/Arm/HitArea
-onready var hitshape := $CelestialWheel/Arm/HitArea/CollisionShape2D
-#onready var ingredient_pos : Vector2 = $Ingredient.position
-#onready var ingredient_size : Vector2 = $Ingredient.get_rect().size
-onready var center := Vector2(0, 0) 
-#onready var top_left := Vector2(ingredient_pos.x-ingredient_size.x/2, ingredient_pos.y-ingredient_size.y/2)
-#onready var peak := Vector2(ingredient_pos.x-(ingredient_size.x/amplitude_factor), ingredient_pos.y-(ingredient_size.y/amplitude_factor))
+onready var center := Vector2(0, 0)
 
+onready var _1_ : Area2D = $CelestialWheel/_1_/CollisionShape2D/
+onready var _2_ : Area2D = $CelestialWheel/_2_/CollisionShape2D/
+onready var _3_ : Area2D = $CelestialWheel/_3_/CollisionShape2D/
+onready var _4_ : Area2D = $CelestialWheel/_4_/CollisionShape2D/
+onready var _5_ : Area2D = $CelestialWheel/_5_/CollisionShape2D/
+onready var _6_ : Area2D = $CelestialWheel/_6_/CollisionShape2D/
+onready var _7_ : Area2D = $CelestialWheel/_7_/CollisionShape2D/
+onready var _8_ : Area2D = $CelestialWheel/_8_/CollisionShape2D/
+onready var _9_ : Area2D = $CelestialWheel/_9_/CollisionShape2D/
+onready var _10_ : Area2D = $CelestialWheel/_10_/CollisionShape2D/
+onready var _11_ : Area2D = $CelestialWheel/_11_/CollisionShape2D/
+onready var _12_ : Area2D = $CelestialWheel/_12_/CollisionShape2D/ 
+
+# Game variables
 var go := false
 var finished := false
 var accum : float = 0.0
-var l = []
+var selected = []
+var pattern = []
+var position_active = 0
 
 func _ready():
 	for i in range(5):
-		l.append(int(rand_range(0, 60)))
-	# Determine possible cutting points
-	for i in l:
-		var cutpoint = CutPointScene.instance()
-		add_child(cutpoint)
-		var angle2 = i * (360/60)
-		#var dot2 = CutPointScene.instance()
-		cutpoint.name = 'dot2_%d' % (i+1)
-		#dot2.rect_size = Vector2(2,2)
-		var cutpoint_rect_pivot_offset = Vector2(1,1)
-		cutpoint.width = 8.0
-		cutpoint.height = 8.0
-		cutpoint.position = (center-cutpoint_rect_pivot_offset) + Vector2(cos(deg2rad(angle2)), sin(deg2rad(angle2))) * 100
-		#dot2.color = Color('#ffffff')
-		cutpoint.shape.set_extents(Vector2(cutpoint.width, cutpoint.height))
-		#add_child(dot2)
+		pattern.append((int(rand_range(1, 12))))
 	
-	# Sweeping arm
-	#var arm = ColorRect.new()
-	arm.name = 'arm'
-	#arm.rect_size = Vector2(3, 90)
-	print(arm.rect_size)
-	arm.rect_pivot_offset = Vector2(20, 168)
-	var shape = RectangleShape2D.new()
-	shape.extents = Vector2(3, 90)
-	hitshape.set_shape(shape)
-	
-	#arm.rect_position = (center-arm.rect_pivot_offset)
-	#var hit_area_rect_pivot_offset = Vector2(1.5, 190)
-	#hitarea.position = (center)
-	#arm.color = Color('#00bbff')
-	#add_child(arm)
-	
-	# TODO: Set the texture of $Ingredient to whatever we are cutting
-	
-	# Create the Cut Points
-#	for i in range(top_left.x+ingredient_size.x/(num_cuts+1), top_left.x+ingredient_size.x, ingredient_size.x/(num_cuts+1)):
+	# Determine possible hit points
+	print(pattern)
+#	for i in pattern:
 #		var cutpoint = CutPointScene.instance()
 #		add_child(cutpoint)
-#		cutpoint.position = Vector2(i, ingredient_pos.y)
-#		cutpoint.width = cutpoint_width
-#		cutpoint.height = ingredient_size.y/2
+#		var angle2 = i * (360/60)
+#		cutpoint.name = 'dot2_%d' % (i+1)
+#		var cutpoint_rect_pivot_offset = Vector2(1,1)
+#		cutpoint.width = 8.0
+#		cutpoint.height = 8.0
+#		cutpoint.position = (center-cutpoint_rect_pivot_offset) + Vector2(cos(deg2rad(angle2)), sin(deg2rad(angle2))) * 140
 #		cutpoint.shape.set_extents(Vector2(cutpoint.width, cutpoint.height))
-	
-	# Initialize the HitArea and its collision shape
-	#hitarea.position = Vector2(arm.rect_position.x, arm.rect_position.y)
 
+	# Sweeping arm
+	arm.name = 'arm'
+	arm.rect_pivot_offset = Vector2(20, 168)
 	
 	# Initialize the Label
-	label.text = "Click when the boxes\nline up to cut"
+	label.text = "Click on the wheel when over a point!"
 
 func _process(delta):
 	
@@ -86,21 +64,20 @@ func _process(delta):
 		return
 	
 	if go: # Wait for user input before beginning
-		# Now check to see if we're done
 		accum += delta
-		arm.rect_rotation = int(SPEED * accum) % 360
-		finished = true
-		for child in get_children():
-			print(child)
-			if child is CutPoint:
-				if child.cut==false:
-					finished = false
-					break
+		arm.rect_rotation += abs(clamp(sin(accum), 0.3, 1)) * speed * delta * direction
+		finished = false
+		
+		# End the game!
+		if pattern.empty():
+			finished = true
+			
+		# Now check to see if we're done
 		if finished:
-			label.text = "well done."
+			label.text = "Well Done."
 		
 		# Move the HitArea on a sine wave
-		sine_x += hit_area_speed * delta
+		#sine_x += hit_area_speed * delta
 		#print($arm.rect_rotation)
 		#hitarea.rotation = int(hitarea.rotation) % 360
 		#hitarea.position = peak.x + ((sin(sine_x)+1) * ingredient_size.x/amplitude_factor)
@@ -125,16 +102,54 @@ func _input(event):
 		if event.button_index==BUTTON_LEFT \
 		or event.button_index==BUTTON_RIGHT \
 		or event.button_index==BUTTON_MIDDLE:
-			# Cut!
-			var hits = hitarea.get_overlapping_areas()
-			var got_a_hit := false
-			if hits: # if we got a cut
-				for hit in hits:
-					if hit is CutPoint:
-						hit.cut = true
-						print("Cut!")
-						label.text = "nice !"
-						got_a_hit = true
-			if not got_a_hit:
-				print("Nope!")
-				label.text = "miss"
+			print(position_active)
+			if position_active == pattern.back():
+				selected.push_back(pattern.back())
+				pattern.pop_back()
+				print(pattern)
+				# Change direction of arm 
+				direction = -direction
+				label.text = "HIT"
+			else:
+				label.text = "MISS"
+				while !selected.empty():
+					pattern.push_back(selected.pop_back())
+				print(pattern)
+				print(selected)
+
+# Signals
+func _on__1__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 1
+
+func _on__2__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 2
+
+func _on__3__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 3
+
+func _on__4__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 4
+
+func _on__5__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 5
+
+func _on__6__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 6
+
+func _on__7__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 7
+
+func _on__8__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 8
+
+func _on__9__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 9
+
+func _on__10__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 10
+
+func _on__11__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 11
+
+func _on__12__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 12
