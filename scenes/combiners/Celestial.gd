527979d647ab3e75c7bdaaad78fa4788f2de6d88
Jacob <jblazusi@ualberta.ca>
2020-03-25 23:36:07 -0600

    Updated file names, included sprites and visual indicators

--- scenes/combiners/Celestial.gd
+++ scenes/combiners/Celestial.gd
@@ -13,28 +13,46 @@ onready var arm := $Wheel/Arm
 onready var hitarea := $Wheel/Arm/HitArea
 onready var center := Vector2(0, 0)
 
-onready var _1_ : Area2D = $Wheel/_1_/CollisionShape2D/
-onready var _2_ : Area2D = $Wheel/_2_/CollisionShape2D/
-onready var _3_ : Area2D = $Wheel/_3_/CollisionShape2D/
-onready var _4_ : Area2D = $Wheel/_4_/CollisionShape2D/
-onready var _5_ : Area2D = $Wheel/_5_/CollisionShape2D/
-onready var _6_ : Area2D = $Wheel/_6_/CollisionShape2D/
-onready var _7_ : Area2D = $Wheel/_7_/CollisionShape2D/
-onready var _8_ : Area2D = $Wheel/_8_/CollisionShape2D/
-onready var _9_ : Area2D = $Wheel/_9_/CollisionShape2D/
-onready var _10_ : Area2D = $Wheel/_10_/CollisionShape2D/
-onready var _11_ : Area2D = $Wheel/_11_/CollisionShape2D/
-onready var _12_ : Area2D = $Wheel/_12_/CollisionShape2D/ 
+onready var _0_ : Area2D = $Wheel/_0_/Sprite_0_
+onready var _1_ : Area2D = $Wheel/_1_/Sprite_1_
+onready var _2_ : Area2D = $Wheel/_2_/Sprite_2_
+onready var _3_ : Area2D = $Wheel/_3_/Sprite_3_
+onready var _4_ : Area2D = $Wheel/_4_/Sprite_4_
+onready var _5_ : Area2D = $Wheel/_5_/Sprite_5_
+onready var _6_ : Area2D = $Wheel/_6_/Sprite_6_
+onready var _7_ : Area2D = $Wheel/_7_/Sprite_7_
+onready var _8_ : Area2D = $Wheel/_8_/Sprite_8_
+onready var _9_ : Area2D = $Wheel/_9_/Sprite_9_
+onready var _10_ : Area2D = $Wheel/_10_/Sprite_10_
+onready var _11_ : Area2D = $Wheel/_11_/Sprite_11_
 
 # Game variables
+onready var game_nodes = [ 
+	_0_,
+	_1_,
+	_2_,
+	_3_,
+	_4_,
+	_5_,
+	_6_,
+	_7_,
+	_8_,
+	_9_,
+	_10_,
+	_11_,
+]
 var go := false
 var finished := false
 var accum : float = 0.0
 var selected = []
 var pattern = []
-var position_active = 0
+var position_active = -1
 
 func _ready():
+	
+	# Cleanup node sprites
+	_cleanup()
+	
 	# Determine possible hit points
 	_generate()
 
@@ -48,22 +66,20 @@ func _ready():
 func _process(delta):
 	
 	if finished:
+		label.text = "Well Done."
 		return
 	
 	if go: # Wait for user input before beginning
-		accum += delta
-		arm.rect_rotation += abs(clamp(sin(accum), 0.3, 1)) * speed * delta * direction * combo
-		finished = false
 		
-		# End the game!
+		# End the game if the pattern is complete
 		if pattern.empty():
 			finished = true
-			
-		# Now check to see if we're done
-		if finished:
-			label.text = "Well Done."
-	
-		update()
+		else:	
+			game_nodes[pattern.back()].show()
+		
+		# Update Arm
+		accum += delta
+		arm.rect_rotation += abs(clamp(sin(accum), 0.3, 1)) * speed * delta * direction * combo
 
 func _input(event):
 	if finished:
@@ -80,31 +96,39 @@ func _input(event):
 		if event.button_index==BUTTON_LEFT \
 		or event.button_index==BUTTON_RIGHT \
 		or event.button_index==BUTTON_MIDDLE:
-			print(position_active)
+			
 			if position_active == pattern.back():
+				game_nodes[pattern.back()].modulate = Color(0, 1, 0)
 				selected.push_back(pattern.back())
 				pattern.pop_back()
-				print(pattern)
+				
 				# Change direction of arm 
 				direction = -direction
 				combo += 1
-				label.text = "HIT"
+		
 			else:
-				label.text = "MISS"
 				while !selected.empty():
 					pattern.push_back(selected.pop_back())
+				_cleanup()
 				combo = 1
-				print(pattern)
-				print(selected)
+				label.text = "MISS"
 
 # Helper Functions
+func _cleanup():
+	for node in game_nodes:
+		node.modulate = Color(1, 1, 1)
+		node.hide()
+		
 func _generate():
 	while len(pattern) < difficulty:
-		var rand = (randi() % 12) + 1
+		var rand = (randi() % 12)
 		if !pattern.has(rand):
 			pattern.append(rand)
 
 # Signals
+func _on__0__area_shape_entered(area_id, area, area_shape, self_shape):
+	position_active = 0
+
 func _on__1__area_shape_entered(area_id, area, area_shape, self_shape):
 	position_active = 1
 
@@ -137,6 +161,3 @@ func _on__10__area_shape_entered(area_id, area, area_shape, self_shape):
 
 func _on__11__area_shape_entered(area_id, area, area_shape, self_shape):
 	position_active = 11
-
-func _on__12__area_shape_entered(area_id, area, area_shape, self_shape):
-	position_active = 12
